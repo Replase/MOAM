269b0437649da67068c3ba63c7c6f374346c29d2
Replase <75821310+Replase@users.noreply.github.com>
2021-06-10 18:10:25 -0500

    agregue vista de imagen

--- scripts/catalogo.js
+++ scripts/catalogo.js
@@ -1,60 +1,212 @@
 $(Document).ready(function(){
     var imagen='';
     $('#uno').click(function(){
-        imagen+='<img src="img/bichaleco.jpg">'
-        alert(imagen)
+        imagen='<img src="img/chaleco-azul.jpg">'
+        if(imagen=='' == false){
+            var mostrar='<div class="ala">'+
+                            '<div class="imagen">'+
+                                '<div class="contenedor">'+
+                                    imagen+
+                                '</div>'+
+                            '</div>'+
+                            '<div class="iconocerrar">'+
+                                '<i id="btn-close" class="bi bi-x-circle" style="font-size: 2rem; color: black;"></i>'+
+                            '</div>'+
+                       '</div>';
+            $('body').append(mostrar);
+        }
+        $('#btn-close').click(function(){
+            $('.ala').remove();
+        });
     })
     $('#dos').click(function(){
-        imagen+='<img src="img/bichaleco.jpg">'
-        alert(imagen)
+        imagen='<img src="img/chaleco-negro.jpg" id="dos">'
+        if(imagen=='' == false){
+            var mostrar='<div class="ala">'+
+                            '<div class="imagen">'+
+                                '<div class="contenedor">'+
+                                    imagen+
+                                '</div>'+
+                            '</div>'+
+                            '<div class="iconocerrar">'+
+                                '<i id="btn-close" class="bi bi-x-circle" style="font-size: 2rem; color: black;"></i>'+
+                            '</div>'+
+                       '</div>';
+            $('body').append(mostrar);
+        }
+        $('#btn-close').click(function(){
+            $('.ala').remove();
+        });
     })
     $('#tres').click(function(){
-        imagen+='<img src="img/bichaleco.jpg">'
-        alert(imagen)
+        imagen='<img src="img/chaleco-rojo.jpg" id="tres">'
+        if(imagen=='' == false){
+            var mostrar='<div class="ala">'+
+                            '<div class="imagen">'+
+                                '<div class="contenedor">'+
+                                    imagen+
+                                '</div>'+
+                            '</div>'+
+                            '<div class="iconocerrar">'+
+                                '<i id="btn-close" class="bi bi-x-circle" style="font-size: 2rem; color: black;"></i>'+
+                            '</div>'+
+                       '</div>';
+            $('body').append(mostrar);
+        }
+        $('#btn-close').click(function(){
+            $('.ala').remove();
+        });
     })
     $('#cuatro').click(function(){
-        imagen+='<img src="img/bichaleco.jpg">'
-        alert(imagen)
+        imagen='<img src="img/chaleco-rosa.jpg" id="cuatro">'
+        if(imagen=='' == false){
+            var mostrar='<div class="ala">'+
+                            '<div class="imagen">'+
+                                '<div class="contenedor">'+
+                                    imagen+
+                                '</div>'+
+                            '</div>'+
+                            '<div class="iconocerrar">'+
+                                '<i id="btn-close" class="bi bi-x-circle" style="font-size: 2rem; color: black;"></i>'+
+                            '</div>'+
+                       '</div>';
+            $('body').append(mostrar);
+        }
+        $('#btn-close').click(function(){
+            $('.ala').remove();
+        });
     })
     $('#cinco').click(function(){
-        imagen+='<img src="img/bichaleco.jpg">'
-        alert(imagen)
+        imagen='<img src="img/chamarra-gris.jpg" id="cinco">'
+        if(imagen=='' == false){
+            var mostrar='<div class="ala">'+
+                            '<div class="imagen">'+
+                                '<div class="contenedor">'+
+                                    imagen+
+                                '</div>'+
+                            '</div>'+
+                            '<div class="iconocerrar">'+
+                                '<i id="btn-close" class="bi bi-x-circle" style="font-size: 2rem; color: black;"></i>'+
+                            '</div>'+
+                       '</div>';
+            $('body').append(mostrar);
+        }
+        $('#btn-close').click(function(){
+            $('.ala').remove();
+        });
     })
     $('#seis').click(function(){
-        imagen+='<img src="img/bichaleco.jpg">'
-        alert(imagen)
+        imagen='<img src="img/chamarra-negra.jpg" id="seis">'
+        if(imagen=='' == false){
+            var mostrar='<div class="ala">'+
+                            '<div class="imagen">'+
+                                '<div class="contenedor">'+
+                                    imagen+
+                                '</div>'+
+                            '</div>'+
+                            '<div class="iconocerrar">'+
+                                '<i id="btn-close" class="bi bi-x-circle" style="font-size: 2rem; color: black;"></i>'+
+                            '</div>'+
+                       '</div>';
+            $('body').append(mostrar);
+        }
+        $('#btn-close').click(function(){
+            $('.ala').remove();
+        });
     })
     $('#siete').click(function(){
-        imagen+='<img src="img/bichaleco.jpg">'
-        alert(imagen)
+        imagen='<img src="img/chamarra-roja.jpg" id="siete">'
+        if(imagen=='' == false){
+            var mostrar='<div class="ala">'+
+                            '<div class="imagen">'+
+                                '<div class="contenedor">'+
+                                    imagen+
+                                '</div>'+
+                            '</div>'+
+                            '<div class="iconocerrar">'+
+                                '<i id="btn-close" class="bi bi-x-circle" style="font-size: 2rem; color: black;"></i>'+
+                            '</div>'+
+                       '</div>';
+            $('body').append(mostrar);
+        }
+        $('#btn-close').click(function(){
+            $('.ala').remove();
+        });
     })
     $('#ocho').click(function(){
-        imagen+='<img src="img/bichaleco.jpg">'
-        alert(imagen)
+        imagen='<img src="img/chamarra-rosa.jpg" id="ocho">'
+        if(imagen=='' == false){
+            var mostrar='<div class="ala">'+
+                            '<div class="imagen">'+
+                                '<div class="contenedor">'+
+                                    imagen+
+                                '</div>'+
+                            '</div>'+
+                            '<div class="iconocerrar">'+
+                                '<i id="btn-close" class="bi bi-x-circle" style="font-size: 2rem; color: black;"></i>'+
+                            '</div>'+
+                       '</div>';
+            $('body').append(mostrar);
+        }
+        $('#btn-close').click(function(){
+            $('.ala').remove();
+        });
     })
     $('#nueve').click(function(){
-        imagen+='<img src="img/bichaleco.jpg">'
-        alert(imagen)
+        imagen='<img src="img/dino-brown.jpg" id="nueve">'
+        if(imagen=='' == false){
+            var mostrar='<div class="ala">'+
+                            '<div class="imagen">'+
+                                '<div class="contenedor">'+
+                                    imagen+
+                                '</div>'+
+                            '</div>'+
+                            '<div class="iconocerrar">'+
+                                '<i id="btn-close" class="bi bi-x-circle" style="font-size: 2rem; color: black;"></i>'+
+                            '</div>'+
+                       '</div>';
+            $('body').append(mostrar);
+        }
+        $('#btn-close').click(function(){
+            $('.ala').remove();
+        });
     })
     $('#diez').click(function(){
-        imagen+='<img src="img/bichaleco.jpg">'
-        alert(imagen)
+        imagen='<img src="img/dino-pink.jpg" id="diez">'
+        if(imagen=='' == false){
+            var mostrar='<div class="ala">'+
+                            '<div class="imagen">'+
+                                '<div class="contenedor">'+
+                                    imagen+
+                                '</div>'+
+                            '</div>'+
+                            '<div class="iconocerrar">'+
+                                '<i id="btn-close" class="bi bi-x-circle" style="font-size: 2rem; color: black;"></i>'+
+                            '</div>'+
+                       '</div>';
+            $('body').append(mostrar);
+        }
+        $('#btn-close').click(function(){
+            $('.ala').remove();
+        });
     })
     $('#once').click(function(){
-        imagen+='<img src="img/bichaleco.jpg">'
-        alert(imagen)
-    })
-    if(imagen=='' == false){
-        var mostrar='<div class="ala">'+
-                        '<div class="imagen">'+
-                            '<div class="contenedor">'+
-                                imagen+
+        imagen='<img src="img/bichaleco.jpg" id="once">'
+        if(imagen=='' == false){
+            var mostrar='<div class="ala">'+
+                            '<div class="imagen">'+
+                                '<div class="contenedor">'+
+                                    imagen+
+                                '</div>'+
                             '</div>'+
-                        '</div>'+
-                        '<div class="iconocerrar">'+
-                            '<i id="btn-close" class="bi bi-x-circle" style="font-size: 2rem; color: black;"></i>'+
-                        '</div>'+
-                   '</div>';
-        $('body').append(mostrar);
-    }
+                            '<div class="iconocerrar">'+
+                                '<i id="btn-close" class="bi bi-x-circle" style="font-size: 2rem; color: black;"></i>'+
+                            '</div>'+
+                       '</div>';
+            $('body').append(mostrar);
+        }
+        $('#btn-close').click(function(){
+            $('.ala').remove();
+        });
+    })
 })
